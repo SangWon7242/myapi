f10b93ed04b713796d5869b47416e94bbe06ab03
SangWon7242 <SangWon7242@users.noreply.github.com>
2026-04-29 14:52:53 +0900

    test: add question subject like query case

--- backend/tests/test_question_db.py
+++ backend/tests/test_question_db.py
@@ -65,8 +65,36 @@ def test_read_question_by_id_1(db_session):
         content="pybo에 대해서 알고 싶습니다.",
     )
 
-    question = db_session.query(Question).filter(Question.id == 1).one()
+    # 조건 검색으로 접근할 때 적합
+    # question = db_session.query(Question).filter(Question.id == 1).one()
+
+    # PK로 접근할 때 적
+    question = db_session.get(Question, 1)
 
     assert question.id == 1
     assert question.subject == "pybo가 무엇인가요?"
     assert question.content == "pybo에 대해서 알고 싶습니다."
+
+
+def test_read_question_subject_like_keyword(db_session):
+    create_question(
+        db_session,
+        subject="pybo가 무엇인가요?",
+        content="pybo에 대해서 알고 싶습니다.",
+    )
+
+    create_question(
+        db_session,
+        subject="FastAPI 모델 질문입니다.",
+        content="id는 자동으로 생성되나요?",
+    )
+
+    questions = (
+        db_session.query(Question)
+        .filter(Question.subject.like("%FastAPI%"))
+        .all()
+    )
+
+    assert len(questions) == 1
+    assert questions[0].subject == "FastAPI 모델 질문입니다."
+
